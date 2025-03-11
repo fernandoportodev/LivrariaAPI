769b4b90cb50679ec27dcf731812bd8721698a4d
Fernando <fernandogomes143@gmail.com>
2025-03-11 03:48:07 -0300

    Criação e ajuste do Repository para implementação do Controller Livrocom a criação dos endpoints

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,65 @@
+﻿using GestaoDeLivraria.Model;
+using GestaoDeLivraria.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeLivraria.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _repository = new();
+
+        public LivroController(LivroRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult CriarLivro([FromBody] Livro livro)
+        {
+            _repository.Adicionar(livro);
+            return CreatedAtAction(nameof(ObterLivroPorId), new {id = livro.Id}, livro);
+        }
+
+        [HttpGet]
+        public IActionResult BuscarTodosLivros()
+        {
+            var livros = _repository.ObterLivros();
+            return Ok(livros);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult ObterLivroPorId(int id)
+        {
+            var livro = _repository.ObterLivroPorId(id);
+            if (livro == null) return NotFound("Livro não encontrado.");
+            return Ok(livro);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult EditarLivro(int id, [FromBody] Livro livro)
+        {
+            var livroExistente = _repository.ObterLivroPorId(id);
+            if (livroExistente == null) return NotFound("Livro não encontrado.");
+
+            livro.Id = id;
+            _repository.EditarLivro(id, livro);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeletarLivro(int id)
+        {
+            var livro = _repository.ObterLivroPorId(id);
+            if (livro == null) return NotFound("Livro não encontrado");
+
+            _repository.DeletarLivro(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/LivroRepository.cs
@@ -0,0 +1,21 @@
+﻿using GestaoDeLivraria.Model;
+
+namespace GestaoDeLivraria.Repositories;
+
+public class LivroRepository
+{
+    private List<Livro> _livros = new List<Livro>(); 
+
+    public void Adicionar(Livro livro) => _livros.Add(livro);
+
+    public List<Livro> ObterLivros() => _livros;
+
+    public Livro ObterLivroPorId(int id) => _livros.FirstOrDefault(l => l.Id == id);
+
+    public void EditarLivro(int id, Livro livro)
+    {
+        var index = _livros.FindIndex(l => l.Id == id);
+        if (index != -1) _livros[index] = livro;
+    }
+    public void DeletarLivro(int id) => _livros.RemoveAll(l => l.Id == id);
+}
